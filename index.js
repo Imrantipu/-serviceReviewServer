e16dc93ee71de21dd027f3a83e3a77ec799dfc00
Imran <imrantipu90@yahoo.com>
2023-01-10 18:35:59 +0100

    database user created and connected in the app

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z2xprxb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try
     {
-
+        const serviceCollection = client.db('emigrationHook').collection('services');
     }
     finally {
 
