40609769e9e5f2082c9fd87d05b60847574fb5fd
Imran <imrantipu90@yahoo.com>
2023-01-10 23:36:24 +0100

    api for adding single service

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
+        app.get('/addservice/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id : ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
 
 
     }
